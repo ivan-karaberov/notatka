99c6dea0441abcd877589ae3bad33d119ea3df4e
ivan-karaberov <karaberov.ivan@yandex.ru>
2024-09-29 20:14:27 +0300

    feat: add redis

--- src/core/config.py
+++ src/core/config.py
@@ -20,6 +20,11 @@ class DbSettings(BaseSettings):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
 
+class RedisSettings(BaseSettings):
+    REDIS_HOST: str
+    REDIS_PORT: int
+
+
 class AuthJWT(BaseSettings):
     private_key_path: Path = BASE_DIR / "core" / "certs" / "jwt-private.pem"
     public_key_path: Path = BASE_DIR / "core" / "certs" / "jwt-public.pem"
@@ -30,6 +35,7 @@ class AuthJWT(BaseSettings):
 
 class Settings(BaseSettings):
     db: DbSettings = DbSettings()
+    redis: RedisSettings = RedisSettings()
     auth_jwt: AuthJWT = AuthJWT()
 
 
