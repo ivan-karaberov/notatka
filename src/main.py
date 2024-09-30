74850005c7e1ade067a64a000411a3a1ee94c72d
ivan-karaberov <karaberov.ivan@yandex.ru>
2024-09-30 08:49:10 +0300

    feat: add custom exception handler

--- src/main.py
+++ src/main.py
@@ -1,9 +1,20 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 
 from api.routers import router as api_v1_router
+from errors.api_errors import APIException
 
 app = FastAPI()
+
+@app.exception_handler(APIException)
+async def custom_exception_handler(request: Request, exc: APIException):
+    """Обработчик собственных исключений"""
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.detail}
+    )
+
 
 app.include_router(api_v1_router, prefix="/api")
 
