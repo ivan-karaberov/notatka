261ebd16aa2723fb1604d9b1f8b4b6c751285584
ivan-karaberov <karaberov.ivan@yandex.ru>
2024-10-22 21:29:56 +0300

    feat: add error details

--- notes/src/errors/api_errors.py
+++ notes/src/errors/api_errors.py
@@ -1,6 +1,34 @@
+from fastapi import status
+
+
 class APIException(Exception):
     status_code = None
     detail = None
 
     def __str__(self):
         return f"{self.detail}"
+    
+
+class NoteNotSavedException(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    detail = "Note not saved"
+
+
+class UserNotHavePermissions(APIException):
+    status_code = status.HTTP_403_FORBIDDEN
+    detail = "User does not have permissions"
+
+
+class NoteNotFoundException(APIException):
+    status_code = status.HTTP_403_FORBIDDEN
+    detail = "Note not found"
+
+
+class NoteNotUpdatedException(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    deatil = "Note not updated"
+
+
+class NoteNotDeletedException(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    detail = "Note not deleted"
